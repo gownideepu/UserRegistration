f3044f3a75c1cb8459ce90e0e6941bd78779908a
unknown <gownideepu23@gmail.com>
2023-02-12 17:48:36 +0530

    added

--- src/main/java/Com/BridgeLabs/UserRegistation.java
+++ src/main/java/Com/BridgeLabs/UserRegistation.java
@@ -5,11 +5,6 @@ import java.util.regex.Pattern;
 
 public class UserRegistation {
     public void UserFirstName(String Firstname) {
-        /*
-         * the A-Z lattters 1 or more capital latters  and a|-z more than 2 latters
-         * has to be taken
-         */
-
         Pattern FirstNamepattern = Pattern.compile("^[A-Z]+[a-z]{2,}$");
         Matcher matcher = FirstNamepattern.matcher(Firstname);
         if (matcher.matches()) {
@@ -27,16 +22,6 @@ public class UserRegistation {
             System.out.println(Lastname +" is Invalid");
         }
     }
-    public void UserEmail(String Email) {
-        Pattern EmailPattern = Pattern.compile("^[a-z]{3}[.]{1}[a-zA-z]{1,}+@+[a-z]{2,}[.]{1}+co+[.]?[a-z]{0,}$");
-        Matcher matcher = EmailPattern.matcher(Email);
-        if (matcher.matches()) {
-            System.out.println(Email +" -Valid");
-        } else {
-            System.out.println(Email +" is Invalid");
-        }
-    }
-
 
     public void PhineNumber(String phonNum) {
         Pattern phonNumMatcher = Pattern.compile("^[+]?[0-9]{2}[\\s]{1}[0-9]{10}$");
@@ -47,16 +32,5 @@ public class UserRegistation {
             System.out.println(phonNum +" is Invalid");
         }
     }
-    public void Password(String Password) {
-        Pattern PasswordMatcher = Pattern.compile("^[a-zA-Z0-9]{8,16}");
-        Matcher matcher = PasswordMatcher.matcher(Password);
-        if (matcher.matches()) {
-            System.out.println(Password +" -Valid");
-        } else {
-            System.out.println(Password +" is Invalid");
-        }
-    }
-
 }
-
 
